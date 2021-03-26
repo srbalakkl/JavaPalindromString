467cdf1f580362836efbbc55985fc44d1b9b16de
SR.Bala <sr.bala.kkl@gmail.com>
2021-03-26 14:20:37 +0530

    intial commit

--- src/PalindromeString/PalindromeString.java
+++ src/PalindromeString/PalindromeString.java
@@ -13,7 +13,6 @@ public class PalindromeString {
 
     private static boolean isPalindrome(String str) {
         int i = 0, j = str.length() - 1;
-
         while (i < j) {
             if (str.charAt(i) == str.charAt(j)) {
                 return true;
